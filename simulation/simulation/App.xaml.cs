c3dd8c7964da2120a34fef99f7e2879d817299a0
gorokusan <h-tanzawa3717@outlook.jp>
2024-09-09 16:48:42 +0900

    simulation First commit

---
+++ simulation/simulation/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace simulation
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
---
+++ simulation/simulation/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace simulation
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
